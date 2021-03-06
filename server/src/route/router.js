b8fe4582b7d466f3ea907c92e3a039b57b42ac48
Jherson <jherson.lopez88@gmail.com>
2021-03-06 16:22:07 -0500

    realizando algunas validaciones

--- server/src/route/router.js
+++ server/src/route/router.js
@@ -8,34 +8,64 @@ var storage = multer.diskStorage({
     cb(null, path.join(__dirname, "../public/uploads"));
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname);
-  },
-  fileFilter: function (req, file, cb) {
-    const filetypes = /jpeg|jpg|png|gif/;
-    const minetype = filetypes.test(file.minetype);
-    const extname = filetypes.test(path.extname(file.originalname));
-    if (minetype && extname) {
-      cb(null, true);
-    } else {
-      cb(null, false);
-    }
-    cb(
-      new Error(
-        `Solo se adminten imagenes con algunas de estas extensiones ${filetypes}`
-      )
-    );
+    cb(null, Date.now() + "-" + file.originalname.toLowerCase());
   },
 });
-const upload = multer({ storage: storage }).array("image", [8]);
+
+function fileFilter(req, file, cb) {
+  const types = /jpeg|jpg|png|gif|svg/;
+  const uperTypes = types.toUpperCase();
+  const filetypes = types.concat(uperTypes);
+  /* const filetypes = /jpeg|jpg|png|gif|svg|JPEG|JPG|PNG|GIF|SVG/; */
+  console.log(filetypes);
+  const mimetype = filetypes.test(file.mimetype);
+  const extname = filetypes.test(path.extname(file.originalname));
+  if (mimetype && extname) {
+    return cb(null, true);
+  }
+  cb(
+    new Error(
+      `Solo se adminten imagenes con algunas de estas extensiones ${filetypes}`
+    )
+  );
+}
+
+const upload = multer({
+  storage: storage,
+  fileFilter: fileFilter,
+  limits: { fileSize: 7000000 },
+}).array("image", [8]);
 
 router.get("/", (req, res) => {
   res.render("index");
 });
 
-router.post("/upload", upload, (req, res) => {
+router.post("/upload", function (req, res) {
+  upload(req, res, function (err) {
+    if (err instanceof multer.MulterError) {
+      console.log("MulterError", err);
+      return res.send({
+        error: "El tamaño de la imagen debe ser menor a 3 megas",
+      });
+    } else if (err) {
+      console.log("UnhandledError", err);
+      return res.send({
+        error:
+          "Solo se adminten imagenes con algunas de estas extensiones /jpeg|jpg|png|gif",
+      });
+    }
+    if (err) {
+      return res.sendStatus(403);
+    }
+    console.log("Se subio correctamente", req.files);
+    res.sendStatus(200);
+  });
+});
+
+/* router.post("/upload", upload, (req, res) => {
   console.log(req.files);
 
   res.send("subido");
-});
+}); */
 
 module.exports = router;
