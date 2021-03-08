edd7137bba6c16cfd88e1b425a62eca519798aed
Jherson <jherson.lopez88@gmail.com>
2021-03-07 20:15:42 -0500

    validando los campos

--- server/src/public/js/index.js
+++ server/src/public/js/index.js
@@ -6,12 +6,21 @@ let area = document.querySelector("#area");
 btnNombre.addEventListener("click", () => {
   let valueNombre = nombre.value;
   let valueInsulto = insulto.value;
-  const pattern = new RegExp("|^[a-zA-Z]+(s*[a-zA-Z]*)*[a-zA-Z]+$|");
-  if (pattern.test(valueInsulto)) {
+  const pattern = new RegExp(
+    /^[a-zA-ZáéíóúÁÉÍÓÚñÑ]+(?: [a-zA-ZáéíóúÁÉÍÓÚñÑ]+)*$/
+  );
+  if (!pattern.test(valueInsulto) || !pattern.test(valueNombre)) {
     alert("Porque favor coloca letras nomas");
-  } else if (pattern.test(valueNombre)) {
-    alert("Porque favor coloca letras nomas");
-  } else if (valueInsulto == "" || valueNombre == "") {
+    /* else if (pattern.test(valueNombre)) {
+    alert("Porque favor coloca letras nomas"); */
+  } else if (
+    valueInsulto == null ||
+    valueInsulto.length == 0 ||
+    /^\s+$/.test(valueInsulto) ||
+    valueNombre == null ||
+    valueNombre.length == 0 ||
+    /^\s+$/.test(valueNombre)
+  ) {
     alert("Escriba algun nombre con su insulto");
   } else {
     let code = `Tu victima se llama ${valueNombre} y es ${valueInsulto} ,en conclusion es un conshasumare :)    
